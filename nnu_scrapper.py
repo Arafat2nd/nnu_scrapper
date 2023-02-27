7e685ba5b258cfaad83c6ae76d265fe48b8a93c1
Arafat2nd <S12027621@STU.Najah.edu>
2023-02-27 22:22:03 +0200

    No proxy no love

--- nnu_scrapper.py
+++ nnu_scrapper.py
@@ -1,11 +1,11 @@
 from re import sub
-import json
 
 from bs4 import BeautifulSoup
 import requests
 
 
 def snake_case(s):
+    sub('[^A-Za-z0-9]+', '', s)
     return '_'.join(
         sub('([A-Z][a-z]+)', r' \1',
             sub('([A-Z]+)', r' \1',
@@ -14,7 +14,7 @@ def snake_case(s):
 
 def nnu_scrapper(link_en):
     soup = BeautifulSoup(requests.get(link_en).text, features="html.parser")
-    for j in range(len(soup.find_all('table'))):
+    for j in range(len(soup.find_all('table')) - (1 if free_less == False else 0)):
         if j == 0:
             continue
         tables = soup.find_all('table')[j]
@@ -29,7 +29,7 @@ def nnu_scrapper(link_en):
             if len(row) > 1:
                 values_en.update({snake_case(str(row[1])): row[1]})
     link_en = link_en.replace('en', 'ar', 1)
-    with open(major + '_en.txt', 'w') as convert_file:
+    with open(major + '_en.txt', 'w', encoding='utf-8') as convert_file:
         convert_file.write(str(values_en))
     nnu_scrapper_with_keys(link_en, list(values_en.keys()))
 
@@ -37,7 +37,7 @@ def nnu_scrapper(link_en):
 def nnu_scrapper_with_keys(link_ar, keys):
     soup = BeautifulSoup(requests.get(link_ar).text, features="html.parser")
     index_of_keys = 0
-    for j in range(len(soup.find_all('table'))):
+    for j in range(len(soup.find_all('table')) - (1 if free_less == False else 0)):
         if j == 0:
             continue
         tables = soup.find_all('table')[j]
@@ -52,18 +52,28 @@ def nnu_scrapper_with_keys(link_ar, keys):
             if len(row) > 1:
                 values_ar.update({keys[index_of_keys]: row[1]})
                 index_of_keys += 1
-    with open(major + '_ar.txt', 'w') as convert_file:
+    with open(major + '_ar.txt', 'w', encoding="utf-8") as convert_file:
         convert_file.write(str(values_ar))
+
+
+def has_free():
+    free = title.find_all('h4', class_='text-bold')
+    free_index = free[len(free) - 1].text
+    if free_index.__eq__('Free Courses'):
+        return True
+    return False
 
 
 values_en = {}
 values_ar = {}
 while True:
+    values_en.clear()
+    values_ar.clear()
     link = input('Enter the english link cunt:')
     title = BeautifulSoup(requests.get(link).text, features="html.parser")
-    major = title.find('div', class_='f24 dark-blue margin-btm-md').text
+    holder = title.find('div', class_='f24 dark-blue margin-btm-md').text
+    major = holder.replace('\n', '')
+    free_less = has_free()
     nnu_scrapper(link)
 
     print('Your files are ready lazy asshole\n')
-
-
